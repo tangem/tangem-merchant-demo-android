22a40abf40da7cff990678d96c31ca120e2ce15d
Anton Zhilenkov <azhilenkov@tangem.com>
2020-10-14 19:43:29 +0300

    TTA-10 - added crashlytics

--- app/src/main/java/com/tangem/merchant/application/AppMerchant.kt
+++ app/src/main/java/com/tangem/merchant/application/AppMerchant.kt
@@ -3,6 +3,8 @@ package com.tangem.merchant.application
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.tangem.merchant.BuildConfig
 import com.tangem.merchant.application.network.NetworkChecker
 import ru.dev.gbixahue.eu4d.lib.android.global.log.Log
 import ru.dev.gbixahue.eu4d.lib.android.global.log.TagLogger
@@ -16,8 +18,17 @@ class AppMerchant : Application() {
         super.onCreate()
 
         appInstance = this
+        initCrashlytics()
         NetworkChecker.createInstance(this)
         Log.setLogger(AppLog())
+    }
+
+    private fun initCrashlytics() {
+        val crashlytics = FirebaseCrashlytics.getInstance()
+        crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+        if (crashlytics.didCrashOnPreviousExecution()) {
+//            TODO: need some action
+        }
     }
 
 
